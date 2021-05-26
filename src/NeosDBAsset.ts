2a9a83347de140f98586e535ab258ec178cfb96c
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-26 05:07:12 -0400

    Update NeosDBAsset.ts

--- src/NeosDBAsset.ts
+++ src/NeosDBAsset.ts
@@ -1,7 +1,7 @@
 export class NeosDBAsset {
 	public Hash: string;
 	public Bytes: number;
-	constructor($b: NeosDBAssetJSON) {
+	constructor($b: NeosDBAssetJSON = {} as NeosDBAssetJSON) {
 		this.Hash = $b.hash;
 		this.Bytes = $b.bytes;
 	}
