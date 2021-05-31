02d0aefa64eb6ffedc76b1ab31a681dfa6de515a
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-31 14:35:58 -0400

    Update Friend.ts

--- src/Friend.ts
+++ src/Friend.ts
@@ -1,4 +1,51 @@
-export class Friend {}
+import { FriendStatus } from "./FriendStatus";
+import { UserStatus, UserStatusJSON } from "./UserStatus";
+import { UserProfile, UserProfileJSON } from "./UserProfile";
+export class Friend {
+	FriendUserId: string;
+	OwnerId: string;
+	FriendUsername: string;
+	FriendStatus: FriendStatus;
+	IsAccepted: boolean;
+	UserStatus: UserStatus;
+	LatestMessageTime: Date;
+	Profile: UserProfile;
+	constructor($b: FriendJSON) {
+		this.FriendUserId = $b.id;
+		this.OwnerId = $b.ownerId;
+		this.FriendUsername = $b.friendUsername;
+		this.FriendStatus = $b.friendStatus;
+		this.IsAccepted = $b.isAccepted;
+		this.UserStatus =
+			$b.userStatus instanceof UserStatus
+				? $b.userStatus
+				: new UserStatus($b.userStatus);
+		this.LatestMessageTime = $b.latestMessageTime;
+		this.Profile =
+			$b.profile instanceof UserProfile
+				? $b.profile
+				: new UserProfile($b.profile);
+	}
+	toJSON(): FriendJSON {
+		return {
+			id: this.FriendUserId,
+			ownerId: this.OwnerId,
+			friendUsername: this.FriendUsername,
+			friendStatus: this.FriendStatus,
+			isAccepted: this.IsAccepted,
+			userStatus: this.UserStatus?.toJSON(),
+			latestMessageTime: this.LatestMessageTime,
+			profile: this.Profile?.toJSON(),
+		};
+	}
+}
 export interface FriendJSON {
 	id: string;
+	ownerId: string;
+	friendUsername: string;
+	friendStatus: FriendStatus;
+	isAccepted: boolean;
+	userStatus: UserStatusJSON;
+	latestMessageTime: Date;
+	profile: UserProfileJSON;
 }
