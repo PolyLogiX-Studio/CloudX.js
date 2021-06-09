a716da966348282a781c368d7f154f3bc87c912e
Ethan Snow <bombitmanbomb@gmail.com>
2021-06-09 04:28:57 -0400

    Update CloudVariableManager.ts

--- src/CloudVariableManager.ts
+++ src/CloudVariableManager.ts
@@ -1,13 +1,21 @@
 import { Dictionary } from "@bombitmanbomb/utils";
 //import { CloudVariableIdentity } from "./CloudVariableIdentity";
 import { CloudVariableProxy } from "./CloudVariableProxy";
+import { CloudXInterface } from "./CloudXInterface";
 //TODO Need BaseX Pool
 //TODO Need BatchQuery
 export class CloudVariableManager {
+	public Cloud: CloudXInterface;
 	private _variableProxies: Dictionary<
 		string,
 		CloudVariableProxy
 	> = new Dictionary(); // CloudVariableIdentity as string
 	private _changedVariables: CloudVariableProxy[] = [];
 	//private _readBatch:VariableReadBatchQuery
+	constructor(cloud: CloudXInterface) {
+		this.Cloud = cloud;
+	}
+	public Update(): void {
+		//TODO
+	}
 }
