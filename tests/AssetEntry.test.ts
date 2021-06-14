052f4e7ae253641b5ce0353c3c09e18da39b6e62
Ethan Snow <bombitmanbomb@gmail.com>
2021-06-13 20:04:08 -0400

    Coverage Reporting

--- tests/AssetEntry.test.ts
+++ tests/AssetEntry.test.ts
@@ -8,6 +8,20 @@ const json: AssetEntryJSON<string> = {
 test("Serialization", () => {
   expect(new AssetEntry(json).toJSON()).toEqual(json)
 })
+describe("AssetHash Property", ()=>{
+  test("AssetHash get", () => {
+    const entry = new AssetEntry(json)
+    expect(entry.AssetHash).toBe("abcde")
+  })
+  test("AssetHash Get Invalid", ()=>{
+    expect(()=>{new AssetEntry().AssetHash}).toThrow()
+  })
+  test("AssetHash set", () => {
+    const entry = new AssetEntry(json)
+    entry.AssetHash = "abcde"
+    expect(entry.OwnerId).toBe("A-abcde")
+  })
+})
 test("AssetHash get", () => {
   const entry = new AssetEntry(json)
   expect(entry.AssetHash).toBe("abcde")
