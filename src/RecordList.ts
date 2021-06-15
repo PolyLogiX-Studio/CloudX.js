8d04631521d940a877ee534df4b2d22b3c1c803f
Ethan Snow <bombitmanbomb@gmail.com>
2021-06-15 02:46:22 -0400

    Create RecordList.ts

---
+++ src/RecordList.ts
@@ -0,0 +1,33 @@
+import { List } from "@bombitmanbomb/utils";
+import { Record, RecordJSON } from "./Record";
+export class RecordList {
+	public get Id(): string {
+		return this.Name + "-" + this.Page;
+	}
+	public OwnerId: string;
+	public Name: string;
+	public Page: number;
+	public Records: List<Record>;
+	constructor($b: RecordListJSON = {} as RecordListJSON) {
+		this.OwnerId = $b.ownerId;
+		this.Name = $b.name;
+		this.Page = $b.page;
+		this.Records = List.ToListAs($b.records, Record);
+	}
+	toJSON(): RecordListJSON {
+		return {
+			id: this.Id,
+			ownerId: this.OwnerId,
+			name: this.Name,
+			page: this.Page,
+			records: this.Records?.toJSON() as unknown as RecordJSON[],
+		};
+	}
+}
+export interface RecordListJSON {
+	readonly id?: string;
+	ownerId: string;
+	name: string;
+	page: number;
+	records: RecordJSON[];
+}
