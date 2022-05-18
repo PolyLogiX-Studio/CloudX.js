db2bbb72f46cba42c66d440a6891b5fc7d4ae0da
Ethan Snow <bombitmanbomb@gmail.com>
2022-05-18 06:05:16 -0400

    EnsureConnected SignalR Promise

--- src/Hub/NeosHub.ts
+++ src/Hub/NeosHub.ts
@@ -1,12 +1,18 @@
-import { HubConnection } from "@bombitmanbomb/signalr";
+import { HubConnection, HubConnectionState } from "@bombitmanbomb/signalr";
 import { INeosHubServer } from "../INeosHubServer";
 import { MarkReadBatch } from "../Models/MarkReadBatch";
 import { Message } from "../Cloud/Message";
+import { TimeSpan } from "@bombitmanbomb/utils/lib";
 export class NeosHub implements INeosHubServer {
 	public Hub: HubConnection;
 
 	constructor(hub: HubConnection) {
 		this.Hub = hub;
+	}
+
+	private async EnsureConnectedHub(): Promise<void> {
+		for (let attempts = 10; this.Hub.state != HubConnectionState.Connected && attempts > 0; attempts--)
+			await TimeSpan.Delay(TimeSpan.fromSeconds(1))
 	}
 
 	public Ping(index: number): Promise<void> {
