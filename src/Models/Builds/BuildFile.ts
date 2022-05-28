114281b52d7dd8c4d44288fe3a8f36feaceeb2a5
Ethan Snow <bombitmanbomb@gmail.com>
2022-05-28 01:28:52 -0400

    Update BuildFile.ts

--- src/Models/Builds/BuildFile.ts
+++ src/Models/Builds/BuildFile.ts
@@ -5,14 +5,27 @@
  * @class BuildFile
  */
 export class BuildFile {
+	/**
+	 * Signature (hash) of the file. used for the NeosDB lookup.
+	 * Null if the entry represents a folder
+	 */
 	public Signature: string;
+	/**
+	 * Size of a given file in bytes.
+	 */
+	public SizeBytes: number
+	/**
+	 * Path of the given file / folder in the installed build. includes filename.
+	 * The given NeosDB asset will be placed at this location.
+	 */
 	public Path: string;
 	constructor($b: BuildFileJSON = {} as BuildFileJSON) {
 		this.Signature = $b.signature;
 		this.Path = $b.path;
+		this.SizeBytes = $b.sizeBytes
 	}
 	toJSON(): BuildFileJSON {
-		return { signature: this.Signature, path: this.Path };
+		return { signature: this.Signature, path: this.Path, sizeBytes: this.SizeBytes };
 	}
 }
 /**
@@ -23,5 +36,6 @@ export class BuildFile {
  */
 export interface BuildFileJSON {
 	signature: string;
+	sizeBytes: number
 	path: string;
 }
