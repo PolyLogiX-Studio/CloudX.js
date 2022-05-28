429ead9ae116a434ab0fa87cddb0d41f12a9535b
Ethan Snow <bombitmanbomb@gmail.com>
2022-05-28 01:29:22 -0400

    Create NeosBuildConfiguration.ts

---
+++ src/Models/Builds/NeosBuildConfiguration.ts
@@ -0,0 +1,9 @@
+export class NeosBuildConfiguration {
+
+}
+export interface NeosBuildConfigurationJSON {
+  configId: string
+  ownerId: string
+  neosConfiguration: NeosConfig //TODO NeosConfig
+  splashScreenSignature: string
+}
