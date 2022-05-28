c13c04a1f84e9cb78f07067882e3e1ab7357eaea
Ethan Snow <bombitmanbomb@gmail.com>
2022-05-28 01:28:48 -0400

    Update BuildChange.ts

--- src/Models/Builds/BuildChange.ts
+++ src/Models/Builds/BuildChange.ts
@@ -1,6 +1,7 @@
 import { List } from "@bombitmanbomb/utils";
 import { BuildChangeType } from "../../BuildChangeType";
 import { BuildReporter, BuildReporterJSON } from "../../BuildReporter";
+import { IssueReference, IssueReferenceJSON } from './IssueReference';
 /**
  *Build Changes
  *
@@ -8,15 +9,23 @@ import { BuildReporter, BuildReporterJSON } from "../../BuildReporter";
  * @class BuildChange
  */
 export class BuildChange {
+	public ChangeId: string
 	public Description: string;
 	public Type: BuildChangeType;
 	public WorkInProgress: boolean;
+	public BranchSpecific: boolean
+	public RelatedIssues: List<IssueReference>
 	public GithubIssueNumbers: List<number>;
 	public Reporters: List<BuildReporter>;
 	constructor($b: BuildChangeJSON = {} as BuildChangeJSON) {
+		this.ChangeId = $b.changeId
 		this.Description = $b.description;
 		this.Type = $b.type;
 		this.WorkInProgress = $b.workInProgress;
+		this.BranchSpecific = $b.branchSpecific
+		this.RelatedIssues = $b.relatedIssues instanceof List
+			? $b.relatedIssues
+			: List.ToListAs($b.relatedIssues, IssueReference)
 		this.GithubIssueNumbers =
 			$b.githubIssueNumbers instanceof List
 				? $b.githubIssueNumbers
@@ -28,9 +37,12 @@ export class BuildChange {
 	}
 	toJSON(): BuildChangeJSON {
 		return {
+			changeId: this.ChangeId,
 			description: this.Description,
 			type: this.Type,
 			workInProgress: this.WorkInProgress,
+			branchSpecific: this.BranchSpecific,
+			relatedIssues: this.RelatedIssues.toJSON() as unknown as IssueReferenceJSON[],
 			githubIssueNumbers: this.GithubIssueNumbers?.toJSON(),
 			reporters: this.Reporters?.toJSON() as unknown as BuildReporterJSON[],
 		};
@@ -43,9 +55,12 @@ export class BuildChange {
  * @interface BuildChangeJSON
  */
 export interface BuildChangeJSON {
+	changeId: string;
 	description: string;
 	type: BuildChangeType;
 	workInProgress: boolean;
+	branchSpecific: boolean;
+	relatedIssues: IssueReferenceJSON[] //TODO Other
 	githubIssueNumbers: number[];
 	reporters: BuildReporterJSON[];
 }
