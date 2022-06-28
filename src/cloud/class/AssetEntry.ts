8ff471a4c268db1363aa2c33069585bcff431e55
Ethan Snow <bombitmanbomb@gmail.com>
2022-06-28 15:24:56 -0400

    Update AssetEntry.ts

--- src/cloud/class/AssetEntry.ts
+++ src/cloud/class/AssetEntry.ts
@@ -10,7 +10,7 @@ export class AssetEntry<E> {
 	public Id: string;
 	public OwnerId: string;
 	public ComputeLock: ComputationLock;
-	public Entry: any;
+	public Entry: E;
 	constructor($b: IAssetEntry<E> = {} as IAssetEntry<E>) {
 		this.Id = $b.id;
 		this.OwnerId = $b.ownerId;
